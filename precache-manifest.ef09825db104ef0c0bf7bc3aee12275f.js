8e9b51ff0e6cf8964be521ec0b4783a228d7e6a6
sujata <gunalesujata@gmail.com>
2020-08-07 17:57:30 +0530

    Updates

---
+++ precache-manifest.ef09825db104ef0c0bf7bc3aee12275f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f111f40d21066a13d4691d1e62c183d",
+    "url": "/Movie-App/index.html"
+  },
+  {
+    "revision": "8edbe3e30eeec7a00330",
+    "url": "/Movie-App/static/css/main.b080fce1.chunk.css"
+  },
+  {
+    "revision": "477aca7d30ce5ac12b81",
+    "url": "/Movie-App/static/js/2.7fe71351.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Movie-App/static/js/2.7fe71351.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8edbe3e30eeec7a00330",
+    "url": "/Movie-App/static/js/main.2c6ed334.chunk.js"
+  },
+  {
+    "revision": "ef32881819421e9d599f",
+    "url": "/Movie-App/static/js/runtime-main.88a57c76.js"
+  }
+]);
